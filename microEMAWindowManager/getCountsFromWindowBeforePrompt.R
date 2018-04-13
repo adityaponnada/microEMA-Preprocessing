33b5d8c860a4008756055e6f3e0302a846307938
Aditya Ponnada <ponnada.a@husky.neu.edu>
2018-04-13 17:07:50 -0400

    Read actigraph count files and rename the data frame with 1 sec epoch intervals

--- microEMAWindowManager/getCountsFromWindowBeforePrompt.R
+++ microEMAWindowManager/getCountsFromWindowBeforePrompt.R
@@ -38,3 +38,59 @@ uEMA01AnsweredPrompts <- uEMA01Responses[!uEMA01Responses$ACTIVITY_TYPE == "MISS
 uEMA01AnsweredPrompts <- uEMA01Responses[!uEMA01Responses$ACTIVITY_TYPE == "DISMISSED", ]
 head(uEMA01AnsweredPrompts)
 levels(uEMA01AnsweredPrompts$ACTIVITY_TYPE)
+
+#### read the actigraph counts file (1 sec epoch intervals): Ankle
+uEMA01AnkleCounts <- read.csv("C:/Users/Dharam/Downloads/microEMA/StudyFiles/VizDataCheck/uema01_activity/ANKLE/YASEEN_ANKLE (2018-02-02)1secDataTable.csv", 
+                              sep = ",", header = TRUE)
+
+head(uEMA01AnkleCounts)
+
+#### Concatinate date time columns in date time object
+uEMA01AnkleCounts$DATE_TIME_ANKLE <- as.POSIXct(paste(uEMA01AnkleCounts$Date, uEMA01AnkleCounts$Time), 
+                                                   format = "%m/%d/%Y %H:%M:%S")
+
+head(uEMA01AnkleCounts)
+
+#### use only selected columns
+uEMA01AnkleCounts <- uEMA01AnkleCounts[, c("DATE_TIME_ANKLE", "Vector.Magnitude")]
+head(uEMA01AnkleCounts)
+
+
+#### rename these columns
+
+colnames(uEMA01AnkleCounts)[2] <- "COUNTS_MAGNITUDE_ANKLE"
+head(uEMA01AnkleCounts)
+
+
+
+#### read the actigraph counts file (1 sec epoch intervaLS) :  Wrist
+
+uEMA01WristCounts <- read.csv("C:/Users/Dharam/Downloads/microEMA/StudyFiles/VizDataCheck/uema01_activity/WRIST/YASEEN_WRIST (2018-02-02)1secDataTable.csv", 
+                              sep = ",", header = TRUE)
+
+head(uEMA01WristCounts)
+
+#### Concatinate date time columns in date time object
+uEMA01WristCounts$DATE_TIME_ANKLE <- as.POSIXct(paste(uEMA01WristCounts$Date, uEMA01WristCounts$Time), 
+                                                format = "%m/%d/%Y %H:%M:%S")
+
+head(uEMA01WristCounts)
+
+#### use only selected columns
+uEMA01WristCounts <- uEMA01WristCounts[, c("DATE_TIME_ANKLE", "Vector.Magnitude")]
+head(uEMA01WristCounts)
+
+
+#### rename these columns
+
+colnames(uEMA01WristCounts)[2] <- "COUNTS_MAGNITUDE_WRIST"
+head(uEMA01WristCounts)
+
+#### get windowed avergaes from the time before the promt
+timebefore = 60
+
+
+
+
+
+
