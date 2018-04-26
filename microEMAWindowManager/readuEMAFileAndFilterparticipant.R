3690f5bbe202dbc612275c7cb5a149109923797e
Aditya Ponnada <ponnada.a@husky.neu.edu>
2018-04-26 14:39:33 -0400

    Plot first four participants for data check

--- microEMAWindowManager/readuEMAFileAndFilterparticipant.R
+++ microEMAWindowManager/readuEMAFileAndFilterparticipant.R
@@ -7,38 +7,38 @@ library(plyr)
 options(digits.secs=3)
 
 #### get a specific participant from 
-uEMA02Responses <- uEMAAllResponses[uEMAAllResponses$USER_ID == "uema02@micropa_com",]
-head(uEMA02Responses)
-tail(uEMA02Responses)
-nrow(uEMA02Responses)
+uEMA04Responses <- uEMAAllResponses[uEMAAllResponses$USER_ID == "uema04@micropa_com",]
+head(uEMA04Responses)
+tail(uEMA04Responses)
+nrow(uEMA04Responses)
 
 #### Keep only relevant columns
-uEMA02Responses <- uEMA02Responses[, c("USER_ID", "PROMPT_TIME", "ANSWER_TIME", "RESPONSE_TIME", "ACTIVITY_TYPE")]
-head(uEMA02Responses)
+uEMA04Responses <- uEMA04Responses[, c("USER_ID", "PROMPT_TIME", "ANSWER_TIME", "RESPONSE_TIME", "ACTIVITY_TYPE")]
+head(uEMA04Responses)
 
-uEMA02Responses$ANSWER_TIME <- as.POSIXct(uEMA02Responses$ANSWER_TIME, format = "%m/%d/%Y %H:%M:%OS")
-uEMA02Responses$PROMPT_TIME <- as.POSIXct(uEMA02Responses$PROMPT_TIME, format = "%m/%d/%Y %H:%M:%OS")
-class(uEMA02Responses$PROMPT_TIME)
-class(uEMA02Responses$ANSWER_TIME)
+uEMA04Responses$ANSWER_TIME <- as.POSIXct(uEMA04Responses$ANSWER_TIME, format = "%m/%d/%Y %H:%M:%OS")
+uEMA04Responses$PROMPT_TIME <- as.POSIXct(uEMA04Responses$PROMPT_TIME, format = "%m/%d/%Y %H:%M:%OS")
+class(uEMA04Responses$PROMPT_TIME)
+class(uEMA04Responses$ANSWER_TIME)
 
 toMatch <- c("MISSED", "DISMISSED")
-uEMA02AnsweredPrompts <- uEMA02Responses[- grep(paste(toMatch,collapse="|"), 
-                                                uEMA02Responses$ACTIVITY_TYPE),]
+uEMA04AnsweredPrompts <- uEMA04Responses[- grep(paste(toMatch,collapse="|"), 
+                                                uEMA04Responses$ACTIVITY_TYPE),]
 
-uEMA02AnsweredPrompts <- subset(uEMA02AnsweredPrompts, ANSWER_TIME < "2018-02-14 23:00:00")
+uEMA04AnsweredPrompts <- subset(uEMA04AnsweredPrompts, ANSWER_TIME < "2018-02-19 23:00:00")
 
-head(uEMA02AnsweredPrompts)
-levels(uEMA02AnsweredPrompts$ACTIVITY_TYPE)
+head(uEMA04AnsweredPrompts)
+levels(uEMA04AnsweredPrompts$ACTIVITY_TYPE)
 
-uEMA02AnsweredPrompts$ACTIVITY_CODED[uEMA02AnsweredPrompts$ACTIVITY_TYPE == "Sedentary"] <- "1000"
-uEMA02AnsweredPrompts$ACTIVITY_CODED[uEMA02AnsweredPrompts$ACTIVITY_TYPE == "Light/Standing"] <- "1500"
-uEMA02AnsweredPrompts$ACTIVITY_CODED[uEMA02AnsweredPrompts$ACTIVITY_TYPE == "Moderate/Walking"] <- "4000"
-uEMA02AnsweredPrompts$ACTIVITY_CODED[uEMA02AnsweredPrompts$ACTIVITY_TYPE == "Vigorous"] <- "8000"
+uEMA04AnsweredPrompts$ACTIVITY_CODED[uEMA04AnsweredPrompts$ACTIVITY_TYPE == "Sedentary"] <- "1000"
+uEMA04AnsweredPrompts$ACTIVITY_CODED[uEMA04AnsweredPrompts$ACTIVITY_TYPE == "Light/Standing"] <- "1500"
+uEMA04AnsweredPrompts$ACTIVITY_CODED[uEMA04AnsweredPrompts$ACTIVITY_TYPE == "Moderate/Walking"] <- "4000"
+uEMA04AnsweredPrompts$ACTIVITY_CODED[uEMA04AnsweredPrompts$ACTIVITY_TYPE == "Vigorous"] <- "8000"
 
-uEMA02AnsweredPrompts$ANSWER_TIME <- as.POSIXct(uEMA02AnsweredPrompts$ANSWER_TIME, format = "%m/%d/%Y %H:%M:%OS")
-uEMA02AnsweredPrompts$PROMPT_TIME <- as.POSIXct(uEMA02AnsweredPrompts$PROMPT_TIME, format = "%m/%d/%Y %H:%M:%OS")
+uEMA04AnsweredPrompts$ANSWER_TIME <- as.POSIXct(uEMA04AnsweredPrompts$ANSWER_TIME, format = "%m/%d/%Y %H:%M:%OS")
+uEMA04AnsweredPrompts$PROMPT_TIME <- as.POSIXct(uEMA04AnsweredPrompts$PROMPT_TIME, format = "%m/%d/%Y %H:%M:%OS")
 
-uEMA02AnsweredPrompts$ACTIVITY_NUMERIC <- as.numeric(as.character(uEMA02AnsweredPrompts$ACTIVITY_CODED))
-head(uEMA02AnsweredPrompts)
+uEMA04AnsweredPrompts$ACTIVITY_NUMERIC <- as.numeric(as.character(uEMA04AnsweredPrompts$ACTIVITY_CODED))
+head(uEMA04AnsweredPrompts)
 
-class(uEMA02AnsweredPrompts$ACTIVITY_NUMERIC)
+class(uEMA04AnsweredPrompts$ACTIVITY_NUMERIC)
