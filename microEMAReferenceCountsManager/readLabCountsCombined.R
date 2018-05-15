0337f1ecccd2e064fdde9b511e865e9266a33cba
Aditya Ponnada <ponnada.a@husky.neu.edu>
2018-05-15 12:15:16 -0400

    Truncate data frame and get 10, 30, and 60s epoch counts

--- microEMAReferenceCountsManager/readLabCountsCombined.R
+++ microEMAReferenceCountsManager/readLabCountsCombined.R
@@ -32,3 +32,19 @@ AllLabCounts$ANKLE_COUNTS_PER_SECOND <- AllLabCounts$ANKLE_COUNTS_ADDED/AllLabCo
 AllLabCounts$WRIST_COUNTS_PER_SECOND <- AllLabCounts$WRIST_COUNTS_ADDED/AllLabCounts$DURATION
 
 head(AllLabCounts)
+
+
+### Get per second counts for 10, 30, and 60 seconds
+
+AllLabCounts$ANKLE_60 <- AllLabCounts$ANKLE_COUNTS_PER_SECOND*60
+AllLabCounts$ANKLE_30 <- AllLabCounts$ANKLE_COUNTS_PER_SECOND*30
+AllLabCounts$ANKLE_10 <- AllLabCounts$ANKLE_COUNTS_PER_SECOND*10
+
+AllLabCounts$WRIST_60 <- AllLabCounts$WRIST_COUNTS_PER_SECOND*60
+AllLabCounts$WRIST_30 <- AllLabCounts$WRIST_COUNTS_PER_SECOND*30
+AllLabCounts$WRIST_10 <- AllLabCounts$WRIST_COUNTS_PER_SECOND*10
+
+AllLabCounts <- select(AllLabCounts, -TOTAL_ROWS_TEMP_ANKLE)
+AllLabCounts <- select(AllLabCounts, -TOTAL_ROWS_TEMP_WRIST)
+
+head(AllLabCounts)
